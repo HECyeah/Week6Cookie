6bf680d398200fea7d48931346fe38a3a4f1acce
HECyeah <hannahecarter@gmail.com>
2024-06-14 16:14:59 +0100

    change

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # MoreCookies
+Requirments:
+🎯 1. Create state variables to store the current number of cookies and the cookies PerSecond value (useState)
 
-I don't know. 
+🎯 2. Set up a timer to increment the number of cookies by the cookiesPerSecond value (useEffect). Be sure to handle clearing the timer using the useEffect return value.
+
+🎯 3. Set up an array of objects containing the items available for purchase in the store, their cost and their increment increase value. Map through these and create buttons for each. For this you can either write the array yourself, or use fetch with this API cookie clicker upgrade API.
+
+🎯 4. Create a function to handle the purchase of an item. This should check if the user has enough cookies to purchase the item, and if so, subtract the cost of the item from the number of cookies and add the increment value to the cookiesPerSecond value.
+
+Questions: 
+1)	Requirements you met. 1 & 2 just. I would not have come to this conclusion on my own, when I tried to do it without the tutorial all I got was a glitchy mess. 
+2)	Goals that you were not quite able to achieved? All of them.
+3)	If so, could you please tell us what it was that you found difficult about these tasks? using React. 
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,23 @@ import './App.css'
 // Array of objects - The upgrades
 const upgrade = [
   {
+    ID: 1,
+    upgradeName: "Sir Can I Have Some More!",
+    description: "Cookies Per Second Increases by 2",
+    Cost: 100
+  },
+  {
+    ID: 2,
+    upgradeName: "I have the High Ground!",
+    description: "Cookies Per Second Increases by 10",
+    Cost: 1000
+  },
+]
+//.map(({ upgradeName, description, Cost }) => { return <p key={upgradeName}>{email}</p> }). Tried to map usign this methiod (https://stackoverflow.com/questions/41027663/how-to-map-an-array-of-objects-in-react) did not work. 
+
+/* This is also how you don't do this task:
+const upgrade [upgradeName, description, Cost ]= [
+  {
     upgradeName: "Sir Can I Have Some More!",
     description: "Cookies Per Second Increases by 2",
     Cost: 100
@@ -13,9 +30,17 @@ const upgrade = [
     Cost: 1000
   },
 ]
-//.map(({ upgradeName, description, Cost }) => { return <p key={upgradeName}>{email}</p> }). Tried to map usign this methiod (https://stackoverflow.com/questions/41027663/how-to-map-an-array-of-objects-in-react) did not work. 
 
+let upgrade = upgrade.map(function(upgrade){
+  return ;( 
+  <h3 key={upgrade.upgradeName}>Upgrade: {upgrade.upgradeName}</h3>
+  <p>Description: {upgrade.description}</p>
+  <p>Cost: {Cost}</p>;
+      <button> Upgrade Me!</ button>;
+  )
+});
 
+*/
 export default function App() {
   const [cookies, setCookies] = useState(0)
   const [cps, setCps] = useState(1);
@@ -40,7 +65,7 @@ export default function App() {
     setCookies(cookies - 100)
   }
 
-  /* Mapping and function upgrades - failed attempt: 
+  /* Mapping and function upgrades - failed attempt:
   function Upgrade(){
     setCps(cps + 1)
     setCookies(cookies - 100)
@@ -54,7 +79,20 @@ export default function App() {
     </>
     )
   }
-
+  
+  */
+  /* this is also now how you do this:
+    return (
+      <div>
+        <h1>Cookie Clicker Game</h1>
+        <button onClick={increaseCookies}> I am a cookie</button>
+        <p>Cookies: {cookies}</p>
+        <p>Cookies Per Second: {cps}</p>
+        <h3>Upgrades:</h3>
+        <button onClick={() => { map.buyUpgrade }><buyUpgrade1 /></button>
+      </div>
+    )
+  }
   */
 
   return (
@@ -64,8 +102,11 @@ export default function App() {
       <p>Cookies: {cookies}</p>
       <p>Cookies Per Second: {cps}</p>
       <h3>Upgrades:</h3>
-      <button onClick={buyUpgrade1}></button>
-      <button></button>
+      <b>Sir Can I Have Some More!</b>
+      <p>Description: "Cookies Per Second Increases by 2"</p>
+      <p> Cost: 100</p>
+      <button onclick={buyUpgrade1}>Upgrade</button>
     </div>
   )
 }
+
